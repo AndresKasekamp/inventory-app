2bbb1dd6f7f2db4dc2b30a4736989bd017e58de3
Andres Kasekamp <andreskasekamp@gmail.com>
2024-06-23 09:34:42 +0300

    trying something

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 
 const PORT = 8080;
 
+console.log("env", `${process.env.INVENTORY_DATABASE_DNS}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`)
+
 app.use(cors());
 app.use(express.json());
 
