9784b3096301ce8b10376d578f7b2753a7fe677c
Andres Kasekamp <andreskasekamp@gmail.com>
2024-06-23 11:15:51 +0300

    again

--- app/models/Movie.js
+++ app/models/Movie.js
@@ -3,6 +3,7 @@ import { Sequelize, DataTypes } from "sequelize";
 const sequelize = new Sequelize("movies", `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
   host: `${process.env.INVENTORY_DATABASE_DNS}`,
   dialect: "postgres",
+  port: 5432,
   define: {
     timestamps: false,
   },
